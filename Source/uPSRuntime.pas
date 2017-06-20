bd3ebb811109ab66c5f08ebb19ef8f39f2a80733
Chris <ulrich@tcs-germany.de>
2017-06-20 06:52:28 +0200

    fic compiling on fpc 2.6.4

--- Source/uPSRuntime.pas
+++ Source/uPSRuntime.pas
@@ -9389,7 +9389,9 @@ begin
   {$ENDIF}
   RegisterDelphiFunction(@Null, 'Null', cdRegister);
   RegisterDelphiFunction(@VarIsNull, 'VarIsNull', cdRegister);
+  {$IFNDEF FPC}
   RegisterDelphiFunction(@VarType, 'VarType', cdRegister);
+  {$ENDIF}
   {$IFNDEF PS_NOIDISPATCH}
   RegisterDelphiFunction(@IDispatchInvoke, 'IdispatchInvoke', cdregister);
   {$ENDIF}
